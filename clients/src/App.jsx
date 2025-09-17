755fa6bb7d94a6bbd16ca239023d3a3b956b6ef8
ChanTolaRen <renchantola168@gmail.com>
2025-09-17 09:53:18 +0700

    Set Project

---
+++ clients/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <center>
+        <h1>Welcome to Client Side</h1>
+      </center>
+    </>
+  );
+}
+
+export default App;
